8ffbada240124d534d9ccd3d866c12f86c597c94
Ken Yoshizoe <kenyoshizoe@gmail.com>
2022-12-16 22:16:16 +0900

    update andante_bringup

--- andante_bringup/launch/bringup.launch.py
+++ andante_bringup/launch/bringup.launch.py
@@ -6,18 +6,22 @@ from launch.substitutions import LaunchConfiguration, Command
 from launch_ros.actions import Node
 from launch_ros.descriptions import ParameterValue
 
+
 def generate_launch_description():
-    #create2(andante)
+    # create2(andante)
+    create_config = os.path.join(
+        get_package_share_directory("andante_bringup"),
+        "config", "create_driver.yaml")
     create_driver = Node(
         package='create_driver',
         executable='create_driver',
         name='create_driver',
-        output='screen',
         parameters=[
-            {'config': os.path.join(get_package_share_directory("andante_bringup"), "config/create_driver.yaml")}
+            create_config
         ]
     )
-    path_to_urdf= get_package_share_directory('andante_description') + "/urdf/andante.urdf.xacro"
+    path_to_urdf = get_package_share_directory(
+        'andante_description') + "/urdf/andante.urdf.xacro"
     create_2_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -31,6 +35,8 @@ def generate_launch_description():
             }
         ]
     )
+    base = [create_driver, create_2_robot_state_publisher]
+
     # ld06
     ld06_serial_port = DeclareLaunchArgument(
         name='ld06_serial_port',
@@ -48,7 +54,7 @@ def generate_launch_description():
         description='Lidar Frame ID'
     )
     ld06_range_threshold = DeclareLaunchArgument(
-        name='ld06_range_threshold', 
+        name='ld06_range_threshold',
         default_value='0.2',
         description='Range Threshold'
     )
@@ -64,12 +70,22 @@ def generate_launch_description():
             {'range_threshold': LaunchConfiguration("ld06_range_threshold")}
         ]
     )
-    return LaunchDescription([
-        create_driver,
-        create_2_robot_state_publisher,
-        ld06_serial_port,
-        ld06_topic,
-        ld06_frame,
-        ld06_range_threshold,
-        ldlidar
-    ])
+    ld06 = [ld06_serial_port,
+            ld06_topic,
+            ld06_frame,
+            ld06_range_threshold,
+            ldlidar]
+
+    # camera
+    camera_v4l2 = Node(
+        package='v4l2_camera',
+        executable='v4l2_camera_node',
+        name='v4l2_camera',
+        output='screen'
+    )
+
+    base.append(camera_v4l2)
+
+    return LaunchDescription(
+        base
+    )
