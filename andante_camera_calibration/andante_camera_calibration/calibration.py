38542495f1d9dce284591d0e3a96611afc03468b
Ken Yoshizoe <kenyoshizoe@gmail.com>
2022-12-17 21:36:52 +0900

    fix calibration.py

--- andante_camera_calibration/andante_camera_calibration/calibration.py
+++ andante_camera_calibration/andante_camera_calibration/calibration.py
@@ -2,6 +2,7 @@
 import rclpy
 import cv2
 import glob
+import yaml
 import numpy as np
 
 
@@ -10,26 +11,26 @@ def main(args=None):
 
     n = rclpy.create_node("andante_camera_calibration")
 
+    n.declare_parameter("preview")
     n.declare_parameter("img_path")
     n.declare_parameter("dst_path")
-    n.declare_parameter("width")
-    n.declare_parameter("height")
     n.declare_parameter("rows")
     n.declare_parameter("cols")
     n.declare_parameter("grid_size")
 
-    rows = n.get_parameter("rows")
-    cols = n.get_parameter("cols")
-    grid_size = n.get_parameter("grid_size")
+    preview = n.get_parameter("preview").value
+    rows = n.get_parameter("rows").value
+    cols = n.get_parameter("cols").value
+    grid_size = n.get_parameter("grid_size").value
 
     chess_pattern = np.zeros((rows * cols, 3), np.float32)
     chess_pattern[:, :2] = np.mgrid[0:rows * grid_size:grid_size,
                                     0:cols*grid_size:grid_size].T.reshape(-1, 2)
 
-    print(chess_pattern)
+    img_path = n.get_parameter("img_path").value
 
-    img_path = n.get_parameter("img_path")
-
+    width = -1
+    height = -1
     obj_points = []
     img_points = []
 
@@ -37,28 +38,51 @@ def main(args=None):
         img = cv2.imread(path)
         if img is None:
             continue
+        if width == -1 or height == -1:
+            height = img.shape[0]
+            width = img.shape[1]
 
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        ret, corners = cv2.findChessboardCorners(img, (rows, cols), None)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        ret, corners = cv2.findChessboardCorners(gray, (rows, cols), None)
 
-        if ret is None:
+        if not ret:
             continue
 
         criteria = (cv2.TERM_CRITERIA_EPS +
                     cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-        corners2 = cv2.cornerSubPix(img, corners, (11, 11), (-1, -1), criteria)
+        corners2 = cv2.cornerSubPix(
+            gray, corners, (11, 11), (-1, -1), criteria)
+
+        if preview:
+            cv2.imshow("preview - hit any key to proceed",
+                       cv2.drawChessboardCorners(img, (rows, cols), corners2, None))
+            key = cv2.waitKey(0)
+            if key == 'n':
+                continue
 
         obj_points.append(chess_pattern)
         img_points.append(corners2)
 
-    width = n.get_parameter("width")
-    height = n.get_parameter("height")
     ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(
         obj_points, img_points, (width, height), None, None)
 
-    dst_path = n.get_parameter("dst_path")
+    dst_path = n.get_parameter("dst_path").value
+    with open(dst_path, 'w') as f:
+        result = {
+            'width': width,
+            'height': height,
+            'camera_matrix': {
+                'fx': float(mtx[0][0]),
+                'fy': float(mtx[1][1]),
+                'cx': float(mtx[0][2]),
+                'cy': float(mtx[1][2])
+            },
+            'distCoeffs': dist.tolist()
+        }
+        yaml.dump(result, f)
 
     rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
