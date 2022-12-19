533387f6b33c2196f0412b1ad43d785f33307f29
Ken Yoshizoe <kenyoshizoe@gmail.com>
2022-12-19 20:05:39 +0900

    add world of petitcon

--- andante_gazebo/setup.py
+++ andante_gazebo/setup.py
@@ -1,16 +1,29 @@
+import os
 from setuptools import setup
+from itertools import chain
 
 package_name = 'andante_gazebo'
+
+
+def add_data_files(data_files):
+    data_dirs = ('models', 'worlds', 'launch')
+    for path, dirs, files in chain.from_iterable(os.walk(data_dir) for data_dir in data_dirs):
+        install_dir = os.path.join('share', package_name, path)
+        list_entry = (install_dir, [os.path.join(path, f)
+                      for f in files if not f.startswith('.')])
+        data_files.append(list_entry)
+    return data_files
+
 
 setup(
     name=package_name,
     version='0.0.0',
     packages=[package_name],
-    data_files=[
+    data_files=add_data_files([
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
+        ('share/' + package_name, ['package.xml'])
+    ]),
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='ken',
